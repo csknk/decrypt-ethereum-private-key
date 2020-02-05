412d233b790737b2419694e471fbc309c17a3d97
David Egan <david@carawebs.com>
2020-02-05 19:21:23 +0000

    Add copyright notice.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Copyright 2020 David Egan
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+
 extern crate serde_json;
 extern crate aes_ctr;
 mod utilities;
