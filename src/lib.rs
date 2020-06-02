760b738edde9e87c17a03bf8202ca9f34636f672
David Egan <david@carawebs.com>
2020-06-02 19:29:31 +0100

    Refactor, move logic out of main

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+// Copyright 2020 David Egan
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+
+extern crate serde_json;
+extern crate aes_ctr;
+mod utilities;
+mod data_process;
+mod decrypt;
+use std::fs;
+use std::io;
+use std::io::Error;
+use crate::utilities::bytes_to_hexstring;
+use crate::data_process::Data;
+use crate::decrypt::derive_key;
+use crate::decrypt::check_key;
+use crate::decrypt::decrypt;
+
+pub struct Config {
+    pub filepath: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 2 {
+            let msg = format!("Usage: {} <path to JSON file>", args[0]);
+            return Err(msg)
+        }
+        let filepath = args[1].clone();
+        Ok(Config {filepath})
+    }
+}
+
+fn read_json(data: String) -> serde_json::Result<serde_json::value::Value> {
+    let v: serde_json::Value = serde_json::from_str(&data)?;
+    Ok(serde_json::json!{v})
+}
+
+fn read_password() -> Result<String, &'static str> {
+    println!("Please enter the password to decrypt the keyfile:");
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)
+        .expect("Failed to read password.");
+    let password = line.trim();
+    Ok(password.to_string())    
+}
+
+pub fn run(config: Config) -> Result<(), Error> {
+    let password = read_password().unwrap();
+    let contents = fs::read_to_string(config.filepath)?;
+    let raw_data = read_json(contents).unwrap();
+    let mut data = Data::new(raw_data); 
+    data.password = password.into_bytes();
+    
+    let key = derive_key(&data).unwrap();
+    if !check_key(&data, &key) {
+        eprintln!("Wrong password");
+        std::process::exit(1);
+    }
+    let plaintext: Vec<u8> = decrypt(&data, &key).unwrap();
+    println!("{}", bytes_to_hexstring(&plaintext));
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -12,61 +12,24 @@
 // use crate::utilities::hexstring_to_bytes;
 // let key_hexstr = "5ae6f8785337645b7cedd53f712863b70cc0615f48f18a3e27a8f922edc13a84";
 // let key = hexstring_to_bytes(key_hexstr.to_string()).unwrap();
-    
 
-extern crate serde_json;
-extern crate aes_ctr;
-mod utilities;
-mod data_process;
-mod decrypt;
 use std::env;
-use std::fs;
-use std::io;
-use std::io::{Error, ErrorKind};
-use crate::utilities::bytes_to_hexstring;
-use crate::data_process::Data;
-use crate::decrypt::derive_key;
-use crate::decrypt::check_key;
-use crate::decrypt::decrypt;
+use std::process;
+use decrypt_ethereum_private_key::Config;
+use decrypt_ethereum_private_key::run;
 
-fn usage_line(name: &String) -> String {
-    let msg = format!("Usage: {} <path to JSON file>", name);
-    return msg;
-}
-
-fn read_json(data: String) -> serde_json::Result<serde_json::value::Value> {
-    let v: serde_json::Value = serde_json::from_str(&data)?;
-    Ok(serde_json::json!{v})
-}
-
-fn read_password() -> Result<String, &'static str> {
-    println!("Please enter the password to decrypt the keyfile:");
-    let mut line = String::new();
-    io::stdin().read_line(&mut line)
-        .expect("Failed to read password.");
-    let password = line.trim();
-    Ok(password.to_string())    
-}
-
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        eprintln!("{}", usage_line(&args[0]));
-        return Err(Error::new(ErrorKind::InvalidInput, "No file specified."));
+
+    let config = match Config::new(&args) {
+        Ok(config) => config,
+        Err(e) => return Err(e),
+    };
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
-    let password = read_password().unwrap();
-    let filename = &args.get(1);
-    let contents = fs::read_to_string(filename.unwrap())?;
-    let raw_data = read_json(contents).unwrap();
-    let mut data = Data::new(raw_data); 
-    data.password = password.into_bytes();
-    
-    let key = derive_key(&data).unwrap();
-    if !check_key(&data, &key) {
-        eprintln!("Wrong password");
-        std::process::exit(1);
-    }
-    let plaintext: Vec<u8> = decrypt(&data, &key).unwrap();
-    println!("{}", bytes_to_hexstring(&plaintext));
+ 
     Ok(())
 }
